46ae3317e30d90cf01c5ce0a55eb097a1134c8d7
alexsuse <alexsusemihl@gmail.com>
2013-06-04 14:56:23 +0200

    Retweets and friends anyone talking about crappy weather, requires python-twitter 1.0.0

--- twitbot.py
+++ twitbot.py
@@ -3,4 +3,13 @@ import credentials as cr
 
 api = twitter.Api(consumer_key=cr.consumer_key, consumer_secret=cr.consumer_secret, access_token_key=cr.token_key, access_token_secret=cr.token_secret)
 
-api.PostUpdate('Gotta hate myself this weather! DAMN! #wtfweather')
+statuses = api.GetSearch("crappy weather")
+
+for i in statuses:
+	try:
+		api.PostRetweet(i.id)
+		print 'retweeting this!'
+		api.CreatFriendship(i.user.id)
+	except twitter.TwitterError:
+		print 'must have retweeted this already!'
+
