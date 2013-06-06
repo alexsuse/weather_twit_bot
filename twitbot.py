57e6117740ebe9faee96fdc4212f56a09ca6ec93
alexsuse <alexsusemihl@gmail.com>
2013-06-06 10:41:12 +0200

    multiple search terms! crazy sheet\!

--- twitbot.py
+++ twitbot.py
@@ -4,7 +4,12 @@ import credentials as cr
 
 api = twitter.Api(consumer_key=cr.consumer_key, consumer_secret=cr.consumer_secret, access_token_key=cr.token_key, access_token_secret=cr.token_secret)
 
-statuses = api.GetSearch("crappy weather")
+search_terms = ["crappy weather","shitty weather","fuck this weather","wtf weather","hate this weather"]
+
+statuses = []
+
+for s in search_terms:
+	statuses = statuses + api.GetSearch(s)
 
 for i in statuses:
 	try:
