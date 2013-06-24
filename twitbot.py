ecff8be9103ea9d00fd5a935d6fca257369660e0
Ubuntu <ubuntu@ip-172-31-22-132.eu-west-1.compute.internal>
2013-06-24 11:50:11 +0000

    retweeting max 20 tweets at each run

--- twitbot.py
+++ twitbot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import twitter
 import credentials as cr
+from numpy.random import permutation
 
 api = twitter.Api(consumer_key=cr.consumer_key, consumer_secret=cr.consumer_secret, access_token_key=cr.token_key, access_token_secret=cr.token_secret)
 
@@ -11,11 +12,12 @@ statuses = []
 for s in search_terms:
 	statuses = statuses + api.GetSearch(s)
 
-for i in statuses:
+perm = permutation(len(s))
+
+for i in xrange(20):
 	try:
-		api.PostRetweet(i.id)
+		api.PostRetweet(statuses[perm[i]].id)
 		print 'retweeting this!'
-#		api.CreateFriendship(i.user.id)
 	except twitter.TwitterError:
 		print 'must have retweeted this already!'
 
