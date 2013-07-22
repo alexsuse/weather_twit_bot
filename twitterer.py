0a6810d95dd7351745c3eafb46a8196ff7e4f61a
Alex Susemihl <alexsusemihl@gmail.com>
2013-07-22 14:32:14 +0000

    twitterer

---
+++ twitterer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+import twitter
+import credentials as cr
+
+api = twitter.Api(consumer_key=cr.consumer_key, consumer_secret=cr.consumer_secret, access_token_key=cr.token_key, access_token_secret=cr.token_secret)
